35bc0917470381744f448f2c46fac8fbe903bd71
kuhn <kuhn_hjh@yeah.net>
2019-04-20 10:44:41 +0800

    新项目

---
+++ springannotate/src/main/java/com/kuhn/spring/test/Test.java
@@ -0,0 +1,10 @@
+package com.kuhn.spring.test;
+
+/**
+ * Created by 10331 on 2019/4/20.
+ */
+public class Test {
+    public  static void main(String[] ages){
+        System.out.print("dfsf");
+    }
+}
